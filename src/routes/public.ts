bdd52b449851c061b8ec0a5ff65363d603105bde
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-03-06 06:44:35 -0500

    feat: Criada a rota de login do usuario

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { z } from "zod";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -14,6 +15,8 @@ const createUserSchema = z.object({
 router.post("/cadastro", async (req, res) => {
   try {
     const { name, email, password } = createUserSchema.parse(req.body);
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt);
 
     const userinDb = await prisma.user.findUnique({
       where: {
@@ -26,7 +29,7 @@ router.post("/cadastro", async (req, res) => {
       });
     }
     const user = await prisma.user.create({
-      data: { name, email, password },
+      data: { name, email, password: hashedPassword },
     });
     res.status(201).json(user);
   } catch (error) {
@@ -43,6 +46,42 @@ router.post("/cadastro", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      res.status(400).json({
+        message: "User not found",
+      });
+    }
+    const passwordIsCorrect = bcrypt.compareSync(password, user!.password!);
+    if (!passwordIsCorrect) {
+      res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+    res.status(200).json(user);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
 export default router;
 
 // michelHoffmann
