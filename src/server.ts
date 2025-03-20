d09604e7c8780a2f9857247b4959e2e8bf9d4146
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-03-20 06:46:13 -0500

    fix: resolvendo erros no retorno da rota /login

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import express from 'express';
+import express from "express";
 import publicRoutes from "./routes/public.ts";
 import privateRoutes from "./routes/private.ts";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-app.use(express.json())
-app.use(cors())
-app.use('/', publicRoutes)
-app.use('/', privateRoutes)
+app.use(express.json());
+app.use(cors());
+app.use("/", publicRoutes);
+app.use("/", privateRoutes);
 
 app.listen(PORT, () => {
-  console.log("🔥 Server is running on port 3000");
+  console.log(`🔥 Server is running on port ${PORT}`);
 });
 
-export default app
+export default app;
